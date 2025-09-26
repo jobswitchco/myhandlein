64e24f759fc0af99f563c10b78df39a10da96a7a
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-09-26 16:53:38 +0530

    Date 26 Sep v1.0

---
+++ backend/models/ProductsCatalogue.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const ProductsCatalogue_Schema = new Schema({
+
+    user_id: { type: Schema.Types.ObjectId, ref: "users", index: true, required: false },
+    title: { type: String, required: false },
+    link: { type: String, required: false },
+    imageUrl: { type: String, required: false },
+    imagePublicId: { type: String, required: false },
+   
+      is_del: {
+    type: Boolean,
+    default: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+  }
+});
+
+const ProductsCatalogue_Schema_Model = mongoose.model("products_catalogue", ProductsCatalogue_Schema);
+export default ProductsCatalogue_Schema_Model;
