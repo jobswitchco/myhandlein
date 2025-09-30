7a73a3a47306fd9abc95d4181b451179c16222a3
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-09-30 18:56:58 +0530

    Date 30 Sep v1.0

--- src/components/Employee/MyChatWindow.js
+++ src/components/Employee/MyChatWindow.js
@@ -111,11 +111,16 @@ export default function MyChatWindow() {
   // socket
   useEffect(() => {
     if (!conversation) return;
-    const socket = io(API_BASE, {
-      transports: ["websocket", "polling"],
-      withCredentials: true,
-      query: { subdomain: conversation?.subdomain || "" }
-    });
+  
+
+       const socket = io("https://myhandle.in", {
+       path: "/socket.io",
+       transports: ["websocket", "polling"], // ok to start with both
+       withCredentials: true, // keep ONLY if you actually rely on cookies (you do for participant token)
+       query: { subdomain },
+     });
+
+     
     socketRef.current = socket;
 
     socket.on("connect", () => {
