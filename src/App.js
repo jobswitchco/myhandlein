6183342e2f04cf1a2ff02afd4634f7bfa74d0c6f
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-10-10 13:07:33 +0530

    Date 10 Oct v1.0

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ import BlocksAnalytics from './components/Employee/BlockAnalytics.js';
 import StoreAnalytics from './components/Employee/StoreAnalytics.js';
 import NewsletterEmailsTable from './components/Employee/NewsletterEmailTable.js';
 import UserSideNavbar from './components/Employee/UserSideNavbar.js'
-// very critial 
+// very critial yes
 
 
 
