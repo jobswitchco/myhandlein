d8e5d0f2d32c2caaac58acaabcd024a1365bd667
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-10-29 14:11:05 +0530

    Date 29 Oct v1.0

--- src/components/Employee/AutomationList.js
+++ src/components/Employee/AutomationList.js
@@ -660,7 +660,7 @@ export default function AutomationList() {
             <Box sx={{ py: 6, display: "flex", justifyContent: "center" }}>
               <CircularProgress />
             </Box>
-          ) : !loading && rowCount === 0 ? (
+          ) : !loading && rows.length === 0 ? (
             <EmptyState onCreate={() => navigate("/professional/fetch_media")} />
           ) : (
             <DataGrid
