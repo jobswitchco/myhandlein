eb36ac0873aaa045882225e465b76def7f8b2427
Jobswitchco <jobswitchco@gmail.com>
2025-12-18 22:46:35 +0530

    Date 18 Dec v1.0

--- src/realtime/redisBridge.js
+++ src/realtime/redisBridge.js
@@ -1,19 +1,23 @@
 // services/redisBridge.js
-import axios from "axios";
+const axios = require("axios");
 
 // const REDIS_BRIDGE_URL = process.env.REDIS_BRIDGE_URL;
 const REDIS_BRIDGE_URL = "http://34.180.49.15:3000";
 
-
-export async function redisGet(key) {
+const redisGet = async (key) => {
   const res = await axios.post(`${REDIS_BRIDGE_URL}/cache/get`, { key });
   return res.data?.value ? JSON.parse(res.data.value) : null;
-}
+};
 
-export async function redisSet(key, value, ttl = 86400) {
+const redisSet = async (key, value, ttl = 86400) => {
   await axios.post(`${REDIS_BRIDGE_URL}/cache/set`, {
     key,
     value,
-    ttl
+    ttl,
   });
-}
+};
+
+module.exports = {
+  redisGet,
+  redisSet,
+};
