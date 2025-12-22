795c4c784066e1cf89fc5a2f8401da417ee240b3
Jobswitchco <jobswitchco@gmail.com>
2025-12-22 16:38:13 +0530

    Date 22 Dec v1.0

--- backend/routes/usersOn.js
+++ backend/routes/usersOn.js
@@ -42,7 +42,7 @@ import { Storage } from '@google-cloud/storage';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import redisBridge from "../../src/realtime/redisBridge.js";
-const { redisGet, redisSet } = redisBridge;
+const { redisGet, redisSet, redisDel } = redisBridge;
 import redis from "../../src/realtime/redis.js";
 
 import igProfileCache from "../../src/realtime/igProfileCache.js";
