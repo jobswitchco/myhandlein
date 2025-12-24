b7184373907c04a805d50746862cfa85f86c594d
Jobswitchco <jobswitchco@gmail.com>
2025-12-24 17:53:17 +0530

    Date 24 Dec v1.0

--- src/components/Employee/TestingInbox.js
+++ src/components/Employee/TestingInbox.js
@@ -845,17 +845,17 @@ const uInitial = uname.charAt(0).toUpperCase();
                 );
 
                 // 2️⃣ If returning to same conversation, force refetch
-                // if (isSameConversation) {
+                if (isSameConversation) {
                   setRawMessages([]);
                   messageIdSetRef.current.clear();
                   setCursor(null);
                   setHasMore(true);
                   await fetchMessages(conv._id);
-                // } else {
+                } else {
                   // 3️⃣ Switch conversation (triggers fetchMessages via useEffect)
-                  // setSelectedConversation(conv);
-                  // setSelectedConversationId(conv._id);
-                // }
+                  setSelectedConversation(conv);
+                  setSelectedConversationId(conv._id);
+                }
 
                 // 4️⃣ Refresh conversation list to update preview
                 const res = await axios.get(`${baseUrl}/conversations`, {
